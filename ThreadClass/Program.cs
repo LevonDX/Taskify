7b57bcec6f09febf4b1bf8b98329a5c82f4b3c8d
Levon Hovhannisyan <lehovhan@microsoft.com>
2023-12-23 12:02:02 +0400

    Add project files.

---
+++ ThreadClass/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ThreadClass
+{
+	internal class Program
+	{
+		static long Sum()
+		{
+			long sum = 0;
+			for (int i = 1; i <= 1E09; i++)
+			{
+				sum += i;
+			}
+
+			return sum;
+		}
+
+		static void Main(string[] args)
+		{
+			Taskify<long> taskify = new Taskify<long>(Sum);
+
+			taskify.Start();
+
+			taskify.ContinueWith(TaskFinished);
+
+			Console.WriteLine("Summing...");
+
+			while (true)
+			{
+				Console.WriteLine("Working");
+				Thread.Sleep(500);
+			}
+		}
+
+		static void TaskFinished(Taskify<long> taskify)
+		{
+			long result = taskify.Result;
+
+			Console.WriteLine("Sum is {0}", result);
+		}
+	}
+}
---
+++ ThreadClass/Taskify.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadClass
+{
+	public class Taskify<T>
+	{
+		private Thread _thread;
+		private T? result;
+
+		public Taskify(Func<T> func)
+		{
+			_thread = new Thread(()=>
+			{
+				result = func();
+			});
+
+			_thread.IsBackground = true;
+		}
+
+		public void Start()
+		{
+			_thread.Start();
+		}
+
+		public static Taskify<T> Run(Func<T> action)
+		{
+			Taskify<T> taskify = new Taskify<T>(action);
+			taskify.Start();
+
+			return taskify;
+		}
+
+		public void Wait()
+		{
+			_thread.Join();
+		}
+
+		public T? Result
+		{
+			get
+			{
+				Wait();
+				return result;
+			}
+		}
+
+		public void ContinueWith(Action<Taskify<T>> action)
+		{
+			Thread thread = new Thread(() =>
+			{
+				while (_thread.IsAlive) ;
+				action(this);
+			});
+
+			thread.IsBackground = true;
+			thread.Start();
+		}
+	}
+}
